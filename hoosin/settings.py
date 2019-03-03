97c2d5d29b62bf313b00d14fef63499e4c931986
Terry <jt2fx@virginia.edu>
2019-03-02 23:27:18 -0500

    requirement.txt change

--- hoosin/settings.py
+++ hoosin/settings.py
@@ -30,10 +30,10 @@ DEBUG = True
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '769440763457-lmmmdrttl5lfmdtrlun4o44p0f7vf1e7.apps.googleusercontent.com'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '1MpnecGGDNVVg13CG_X-56Oz'
 
-LOGIN_URL = 'login'
+LOGIN_URL = '/auth/login/google-oauth2/'
 
 LOGIN_REDIRECT_URL = '/profiles/students'
-#LOGOUT_REDIRECT_URL = '/'
+#LOGOUT_REDIRECT_URL = '/logout'
 
 SOCIAL_AUTH_URL_NAMESPACE = 'social'
 
