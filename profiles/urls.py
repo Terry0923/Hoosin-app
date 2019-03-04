fc86523e505d4072f106ecd7936b34d01c36072f
Terry <35023141+Terry0923@users.noreply.github.com>
2019-03-04 02:55:15 -0500

    Update urls.py

--- profiles/urls.py
+++ profiles/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('students/', views.studentindex, name='student-index'),
     path('clubs/', views.clubindex, name='club-index'),
     path('students/<int:student_id>/', views.detail, name='detail'),
-    path('', include('social_django.urls', namespace='social')),
 ]
