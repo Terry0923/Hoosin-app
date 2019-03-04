f0bacf3bbec39d5c2fab0c0fe7ad022fdabbd176
Terry <35023141+Terry0923@users.noreply.github.com>
2019-03-04 02:57:28 -0500

    Update urls.py

--- hoosin/urls.py
+++ hoosin/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from profiles import views as profile_views
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profiles/', include('profiles.urls')),
+    path('', include('social_django.urls', namespace='social')),
     path('login/',auth_views.LoginView.as_view(template_name='profiles/login.html'), name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='profiles/logout.html'),name='logout'),
     path('profile/',profile_views.profile, name='profile'),
