1a309bb6322d62e757419891e835799ab651129a
Saad Khan <sk4gw@virginia.edu>
2019-03-04 16:35:14 -0500

    login changes

--- hoosin/settings.py
+++ hoosin/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'hoosin.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'hoosin.db'),
     }
 }
 
