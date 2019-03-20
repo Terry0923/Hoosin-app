0a28afd3c34458675aa8789ddcaff0b6338d8f8e
Megan Do <meganvy@gmail.com>
2019-03-20 17:59:47 -0400

    added travis testing files as in testing branch; may need modification

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from .models import Student, Club
+from .forms import UserRegisterForm
+
 
 # Create your tests here.
+class StudentModelTests(TestCase):
+
+    def test_student_object(self):
+       s = Student(name="Gautam", year="fourth", major="Computer Engineering")
+       self.assertEqual(s.name, "Gautam")
+       self.assertEqual(s.year, "fourth")
+       self.assertEqual(s.major, "Computer Engineering")
+
+
+class ClubModelTests(TestCase):
+
+    def test_club_object(self):
+       c = Club(name="ACM", description="club for nerds")
+       self.assertEqual(c.name, "ACM")
+       self.assertEqual(c.description, "club for nerds")
