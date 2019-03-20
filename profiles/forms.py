2c24e31f8aa650527a7b73ff0759e8d74d69bd59
Terry <jt2fx@virginia.edu>
2019-03-20 17:17:34 -0400

    First commit

--- profiles/forms.py
+++ profiles/forms.py
@@ -16,8 +16,9 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'email']
-
+'''
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image']
+'''
--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404
 from .models import Student, Club, Profile
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .forms import UserRegisterForm, UserUpdateForm #ProfileUpdateForm
 from django import forms
 from django.contrib import messages
 
@@ -44,9 +44,9 @@ def register(request):
 def profile(request):
     if request.method == 'POST':
         u_form = UserUpdateForm(request.POST, instance=request.user)
-        p_form = ProfileUpdateForm(request.POST,
-                                   request.FILES,
-                                   instance=request.user.profile)
+        # p_form = ProfileUpdateForm(request.POST,
+        #                           request.FILES,
+        #                           instance=request.user.profile)
         if u_form.is_valid() and p_form.is_valid():
             u_form.save()
             p_form.save()
@@ -55,11 +55,11 @@ def profile(request):
     else:
         user = Profile.objects.get_or_create(user=request.user)
         u_form = UserUpdateForm(instance=request.user)
-        p_form = ProfileUpdateForm(instance=request.user.profile)
+    #   p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form': u_form,
-        'p_form': p_form
+    #   'p_form': p_form
     }
 
     return render(request, 'profiles/profile.html', context)
