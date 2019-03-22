46b405a89ad7e51d7c2b2b0f99189a32bed9649f
Terry <jt2fx@virginia.edu>
2019-03-21 23:17:29 -0400

    First

--- profiles/models.py
+++ profiles/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
 # Create your models here.
 class Student(models.Model):
     #user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
@@ -60,11 +60,10 @@ class Profile(models.Model):
         super().save()
 
 
-'''
+
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
             output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
-'''
