0c2f6e14e1d107649a4e668dae6ef6127134b28d
Terry <jt2fx@virginia.edu>
2019-03-24 15:18:39 -0400

    FIrst

--- hoosin/settings.py
+++ hoosin/settings.py
@@ -150,7 +150,7 @@ MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK='bootstrap4'
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'profiles-static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 ALLOWED_HOSTS = ['testcases123.herokuapp.com', 'localhost', '127.0.0.1']
 
