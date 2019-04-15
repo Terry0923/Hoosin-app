0c1f21e05ae323c68ae2ce0830d3a33ff6ccdf6d
Pauline Vu <ptv6ug@virginia.edu>
2019-04-15 10:48:51 -0400

    reordered the profile update form

--- profiles/forms.py
+++ profiles/forms.py
@@ -31,7 +31,7 @@ class UserUpdateForm(forms.ModelForm):
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['bio', 'image', 'year','major','school']
+        fields = ['image', 'status', 'year','major','school', 'bio']
 
 class PostForm(forms.ModelForm):
     class Meta:
